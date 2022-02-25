f50e0ba562a985f0da4f5e4531b45311fcce9a75
Mateusz Ochman <ochman.m.98@gmail.com>
2022-02-25 16:52:39 +0100

    Moved to another folders

--- Missile_Command/Assets/Scripts/DestroyExplosion.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DestroyExplosion : MonoBehaviour
-{
-    public void DestroyExplosionPrefab(float destroyTime)
-    {
-        Destroy(this.gameObject, destroyTime);
-    }
-}
